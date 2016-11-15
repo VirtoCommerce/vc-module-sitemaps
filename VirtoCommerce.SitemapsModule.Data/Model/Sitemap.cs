53429efc98f7c70e300754c9f83cf17148e57d7e
Andrew Orlov <ao@virtoway.com>
2016-11-15 14:56:51 +0200

    Initial commit

---
+++ VirtoCommerce.SitemapsModule.Data/Model/Sitemap.cs
@@ -0,0 +1,8 @@
+﻿using VirtoCommerce.Platform.Core.Common;
+
+namespace VirtoCommerce.SitemapsModule.Data.Model
+{
+    public class Sitemap : AuditableEntity
+    {
+    }
+}
---
+++ VirtoCommerce.SitemapsModule.Web/Module.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Practices.Unity;
+using VirtoCommerce.Platform.Core.Modularity;
+
+namespace VirtoCommerce.SitemapsModule.Web
+{
+    public class Module : ModuleBase
+    {
+        private const string _connectionStringName = "VirtoCommerce";
+        private readonly IUnityContainer _container;
+
+        public Module(IUnityContainer container)
+        {
+            _container = container;
+        }
+    }
+}
