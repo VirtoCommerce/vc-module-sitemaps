a7e3a83be68111d782dbfa8f8103aa2d9719e092
Tatarincev <et@virtoway.com>
2016-12-23 17:35:47 +0200

    Parallel products loading

--- VirtoCommerce.SitemapsModule.Data/Services/SitemapItemRecordProviders/CatalogSitemapItemRecordProvider.cs
+++ VirtoCommerce.SitemapsModule.Data/Services/SitemapItemRecordProviders/CatalogSitemapItemRecordProvider.cs
@@ -44,13 +44,13 @@ namespace VirtoCommerce.SitemapsModule.Data.Services.SitemapItemRecordProviders
                 Priority = SettingsManager.GetValue("Sitemap.ProductPagePriority", 1.0M),
                 UpdateFrequency = SettingsManager.GetValue("Sitemap.ProductPageUpdateFrequency", UpdateFrequency.Daily)
             };
-            var searchBunchSize = SettingsManager.GetValue("Sitemap.SearchBunchSize", 1000);
+            var searchBunchSize = SettingsManager.GetValue("Sitemap.SearchBunchSize", 500);
 
             var categorySitemapItems = sitemap.Items.Where(x => x.ObjectType.EqualsInvariant(SitemapItemTypes.Category));
             var categoryIds = categorySitemapItems.Select(x => x.ObjectId).ToArray();
             var categories = CategoryService.GetByIds(categoryIds, CategoryResponseGroup.WithSeo);
 
-            foreach (var sitemapItem in categorySitemapItems)
+            Parallel.ForEach(categorySitemapItems, new ParallelOptions { MaxDegreeOfParallelism = 5 }, (sitemapItem =>
             {
                 var category = categories.FirstOrDefault(x => x.Id == sitemapItem.ObjectId);
                 if (category != null)
@@ -64,6 +64,7 @@ namespace VirtoCommerce.SitemapsModule.Data.Services.SitemapItemRecordProviders
                             ResponseGroup = SearchResponseGroup.WithCategories,
                             Skip = 0,
                             Take = searchBunchSize,
+                            HideDirectLinkedCategories = true,
                             SearchInChildren = true
                         };
                         var catalogSearchResult = CatalogSearchService.Search(catalogSearchCriteria);
@@ -77,7 +78,7 @@ namespace VirtoCommerce.SitemapsModule.Data.Services.SitemapItemRecordProviders
                         catalogSearchResult = CatalogSearchService.Search(catalogSearchCriteria);
                         for (int i = 0; i < catalogSearchResult.ProductsTotalCount; i += searchBunchSize)
                         {
-                            foreach(var product in catalogSearchResult.Products)
+                            foreach (var product in catalogSearchResult.Products)
                             {
                                 sitemapItem.ItemsRecords.AddRange(GetSitemapItemRecords(productOptions, sitemap.UrlTemplate, baseUrl, product));
                             }
@@ -86,7 +87,7 @@ namespace VirtoCommerce.SitemapsModule.Data.Services.SitemapItemRecordProviders
                         }
                     }
                 }
-            }
+            }));
 
             var productSitemapItems = sitemap.Items.Where(si => si.ObjectType.EqualsInvariant(SitemapItemTypes.Product));
             var productIds = productSitemapItems.Select(si => si.ObjectId).ToArray();
