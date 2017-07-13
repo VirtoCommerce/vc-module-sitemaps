d94610b6cde938832f968ea1674430cf9096daf0
Eugeny Tatarincev <et@virtoway.com>
2017-07-13 13:12:21 +0200

    [publish] 1.1.3

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("VirtoCommerce Sitemaps Module")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 
-[assembly: AssemblyFileVersion("1.1.2.0")]
-[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
