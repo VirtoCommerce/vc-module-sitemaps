419062317eb88ce837a552da83cbf5037f0480dc
Andrew Orlov <ao@virtoway.com>
2017-09-18 18:06:39 +0200

    Fixed links to project files

--- NuGet/build.bat
+++ NuGet/build.bat
@@ -15,7 +15,7 @@ IF NOT DEFINED MSBUILD_PATH SET MSBUILD_PATH=%WINDIR%\Microsoft.NET\Framework\v4
 
 "%MSBUILD_PATH%" "%SOURCE_DIR%\VirtoCommerce.SitemapsModule.sln" /nologo /verbosity:m /t:Build /p:Configuration=Release;Platform="Any CPU"
 
-nuget pack "%SOURCE_DIR%\VirtoCommerce.SitemapsModule.Core\VirtoCommerce.OrderModule.Core.csproj" -IncludeReferencedProjects -Symbols -Properties Configuration=Release -o "%TARGET_DIR%"
-nuget pack "%SOURCE_DIR%\VirtoCommerce.SitemapsModule.Data\VirtoCommerce.OrderModule.Data.csproj" -IncludeReferencedProjects -Symbols -Properties Configuration=Release -o "%TARGET_DIR%"
+nuget pack "%SOURCE_DIR%\VirtoCommerce.SitemapsModule.Core\VirtoCommerce.SitemapsModule.Core.csproj" -IncludeReferencedProjects -Symbols -Properties Configuration=Release -o "%TARGET_DIR%"
+nuget pack "%SOURCE_DIR%\VirtoCommerce.SitemapsModule.Data\VirtoCommerce.SitemapsModule.Data.csproj" -IncludeReferencedProjects -Symbols -Properties Configuration=Release -o "%TARGET_DIR%"
 
 @pause
