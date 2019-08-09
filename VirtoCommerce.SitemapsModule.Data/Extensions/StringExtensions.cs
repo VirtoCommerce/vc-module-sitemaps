a48c4b608aedbcc5a73544639818cc365097b54a
Sergey Podguzov <spo@virtoway.com>
2019-08-09 20:45:49 +0500

    BE: Build url logic for .page files for sitemap loading.

---
+++ VirtoCommerce.SitemapsModule.Data/Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace VirtoCommerce.SitemapsModule.Data.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsJson(this string source, ref JToken token)
+        {
+            var trimmed = source.Trim();
+            if (trimmed.StartsWith("[") && trimmed.EndsWith("]") ||
+                trimmed.StartsWith("{") && trimmed.EndsWith("}"))
+            {
+                try
+                {
+                    token = JToken.Parse(source);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
