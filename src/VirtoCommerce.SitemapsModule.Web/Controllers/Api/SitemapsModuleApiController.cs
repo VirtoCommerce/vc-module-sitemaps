15e3900f5612bc6d192b7caacd042df68bf09e2b
Filip Kuksov <vectorfield4@gmail.com>
2020-10-08 13:46:18 +0200

    add description

--- src/VirtoCommerce.SitemapsModule.Web/Controllers/Api/SitemapsModuleApiController.cs
+++ src/VirtoCommerce.SitemapsModule.Web/Controllers/Api/SitemapsModuleApiController.cs
@@ -327,7 +327,11 @@ namespace VirtoCommerce.SitemapsModule.Web.Controllers.Api
         [ApiExplorerSettings(IgnoreApi = true)]
         public Task BackgroundDownload(string storeId, string baseUrl, SitemapDownloadNotification notification)
         {
-            if (!Regex.IsMatch(storeId, "^[a-zA-Z0-9]+$")) // storeId.IndexOfAny(Path.GetInvalidFileNameChars()) != -1
+            /* We cannot use storeId.IndexOfAny(Path.GetInvalidFileNameChars()) != -1 to validate path because default
+             * sanitizer for Sonar Cube do not trust it, so we use Regex here with same logic. Check this out
+             * https://community.sonarsource.com/t/help-sonarcloud-with-understanding-the-usage-of-untrusted-and-tainted-input/9873/7
+            */
+            if (!Regex.IsMatch(storeId, "^[a-zA-Z0-9]+$"))
             {
                 throw new ArgumentException($"Incorrect name of store {storeId}");
             }
