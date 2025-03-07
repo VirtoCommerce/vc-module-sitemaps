97436335ab9496b3100e6150f6730207b0b09342
Kutasina Elena <62027488+ekuvirto@users.noreply.github.com>
2025-03-07 09:36:37 +0300

    VCST-2752: Added option to specify SEO links type per sitemap and bugfix on cate… (#81)

--- src/VirtoCommerce.SitemapsModule.Core/Models/UrlTemplatePatterns.cs
+++ src/VirtoCommerce.SitemapsModule.Core/Models/UrlTemplatePatterns.cs
@@ -1,10 +1,13 @@
-﻿namespace VirtoCommerce.SitemapsModule.Core.Models
+namespace VirtoCommerce.SitemapsModule.Core.Models
 {
     public static class UrlTemplatePatterns
     {
         public const string StoreUrl = "{storeUrl}",
                             StoreSecureUrl = "{storeSecureUrl}",
                             Language = "{language}",
-                            Slug = "{slug}";
+                            Slug = "{slug}",
+                            SlugShort = "{slug_short}",
+                            SlugLong = "{slug_long}",
+                            SlugCollapsed = "{slug_collapsed}";
     }
 }
