f017bd8d028d731fcc267536f540c8dd737d2515
Artem Dudarev <artem@virtoworks.com>
2025-04-24 12:46:29 +0200

    Add tests for products with images

--- tests/VirtoCommerce.SitemapsModule.Tests/SitemapXmlGeneratorTests.cs
+++ tests/VirtoCommerce.SitemapsModule.Tests/SitemapXmlGeneratorTests.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Moq;
+using VirtoCommerce.CatalogModule.Core.Model;
 using VirtoCommerce.CatalogModule.Core.Model.ListEntry;
 using VirtoCommerce.CatalogModule.Core.Model.Search;
 using VirtoCommerce.CatalogModule.Core.Search;
 using VirtoCommerce.CatalogModule.Core.Services;
 using VirtoCommerce.CoreModule.Core.Outlines;
 using VirtoCommerce.CoreModule.Core.Seo;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.SitemapsModule.Core.Models;
 using VirtoCommerce.SitemapsModule.Core.Models.Search;
@@ -21,6 +23,7 @@ using VirtoCommerce.StoreModule.Core.Model;
 using VirtoCommerce.StoreModule.Core.Services;
 using Xunit;
 using static VirtoCommerce.CatalogModule.Core.Extensions.SeoExtensions;
+using static VirtoCommerce.SitemapsModule.Core.ModuleConstants.Settings.ProductLinks;
 using static VirtoCommerce.StoreModule.Core.ModuleConstants.Settings.SEO;
 
 namespace VirtoCommerce.SitemapsModule.Tests;
@@ -28,13 +31,18 @@ namespace VirtoCommerce.SitemapsModule.Tests;
 public class SitemapXmlGeneratorTests
 {
     [Theory]
-    [InlineData(SeoShort, "en", "sitemap_short_en.xml")]
-    [InlineData(SeoShort, "en,de", "sitemap_short_en,de.xml")]
-    [InlineData(SeoCollapsed, "en", "sitemap_collapsed_en.xml")]
-    [InlineData(SeoCollapsed, "en,de", "sitemap_collapsed_en,de.xml")]
-    public async Task GenerateSitemapXml_ShouldReturnValidXml(string seoLinksType, string languages, string expectedXmlFileName)
+    [InlineData(SeoShort, "en", false, "sitemap_short_en.xml")]
+    [InlineData(SeoShort, "en,de", false, "sitemap_short_en,de.xml")]
+    [InlineData(SeoCollapsed, "en", false, "sitemap_collapsed_en.xml")]
+    [InlineData(SeoCollapsed, "en,de", false, "sitemap_collapsed_en,de.xml")]
+
+    [InlineData(SeoShort, "en", true, "sitemap_short_en_images.xml")]
+    [InlineData(SeoShort, "en,de", true, "sitemap_short_en,de_images.xml")]
+    [InlineData(SeoCollapsed, "en", true, "sitemap_collapsed_en_images.xml")]
+    [InlineData(SeoCollapsed, "en,de", true, "sitemap_collapsed_en,de_images.xml")]
+    public async Task GenerateSitemapXml_ShouldReturnValidXml(string seoLinksType, string languages, bool withImages, string expectedXmlFileName)
     {
-        // Arrange  
+        // Arrange
         var sitemapGenerator = new SitemapXmlGenerator(
             GetSitemapSearchServiceMock().Object,
             GetSitemapItemSearchServiceMock().Object,
@@ -42,15 +50,15 @@ public class SitemapXmlGeneratorTests
             [GetCatalogSitemapItemRecordProvider()],
             new Mock<ISettingsManager>().Object,
             new Mock<ILogger<SitemapXmlGenerator>>().Object,
-            GetStoreServiceMock(seoLinksType, languages).Object);
+            GetStoreServiceMock(seoLinksType, languages, withImages).Object);
 
         var expectedXml = await GetEmbeddedResourceString($"Resources.{expectedXmlFileName}");
 
-        // Act  
+        // Act
         var stream = await sitemapGenerator.GenerateSitemapXmlAsync("store1", "https://test.local", "products.xml");
         var actualXml = await ReadAsString(stream);
 
-        // Assert  
+        // Assert
         Assert.Equal(expectedXml, actualXml);
     }
 
@@ -80,6 +88,8 @@ public class SitemapXmlGeneratorTests
                     [
                         new SitemapItem { ObjectType = "category", ObjectId = "category1" },
                         new SitemapItem { ObjectType = "category", ObjectId = "category2" },
+                        new SitemapItem { ObjectType = "product", ObjectId = "product1" },
+                        new SitemapItem { ObjectType = "product", ObjectId = "product2" },
                     ],
                 });
 
@@ -91,7 +101,7 @@ public class SitemapXmlGeneratorTests
         return new CatalogSitemapItemRecordProvider(
             GetSitemapUrlBuilder(),
             new Mock<ISettingsManager>().Object,
-            new Mock<IItemService>().Object,
+            GetItemServiceMock().Object,
             GetListEntrySearchServiceMock().Object);
     }
 
@@ -100,7 +110,7 @@ public class SitemapXmlGeneratorTests
         return new SitemapUrlBuilder();
     }
 
-    private static Mock<IStoreService> GetStoreServiceMock(string seoLinksType, string languages)
+    private static Mock<IStoreService> GetStoreServiceMock(string seoLinksType, string languages, bool withImages)
     {
         var storeServiceMock = new Mock<IStoreService>();
 
@@ -112,10 +122,34 @@ public class SitemapXmlGeneratorTests
                 Catalog = "catalog1",
                 DefaultLanguage = "en",
                 Languages = languages.Split(','),
-                Settings = [new ObjectSettingEntry { Name = SeoLinksType.Name, Value = seoLinksType }],
+                Settings = [
+                    new ObjectSettingEntry { Name = SeoLinksType.Name, Value = seoLinksType },
+                    new ObjectSettingEntry { Name = IncludeImages.Name, Value = withImages },
+                ],
             }]);
 
         return storeServiceMock;
+    }
+
+    private static Mock<IItemService> GetItemServiceMock()
+    {
+        var itemServiceMock = new Mock<IItemService>();
+
+        itemServiceMock
+            .Setup(x => x.GetAsync(It.IsAny<IList<string>>(), It.IsAny<string>(), It.IsAny<bool>()))
+            .ReturnsAsync((IList<string> _, string responseGroup, bool _) =>
+            {
+                var product = new CatalogProduct { Id = "product1" };
+
+                if (EnumUtility.SafeParseFlags(responseGroup, ItemResponseGroup.None).HasFlag(ItemResponseGroup.WithImages))
+                {
+                    product.Images = [new Image { Url = "https://images.local/image1.jpg" }];
+                }
+
+                return [product];
+            });
+
+        return itemServiceMock;
     }
 
     private static Mock<IListEntrySearchService> GetListEntrySearchServiceMock()
