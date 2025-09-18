f1f11bbfef5e5a1fd28f174f14642a0e676a5f40
Oleg Zhuk <zhukoo@gmail.com>
2025-09-18 15:20:53 +0200

    VCST-3856: Remove language code from sitemap root files (#86)
    fix: Remove language code from sitemap root files. (#86)
    fix: Remove default store language from path. (#86)
    feat: Added Include default store's language in URL store setting. Enable/disable inclusion of the language code in URLs for the store's default language. When enabled, URLs will contain the language code even for the default language (e.g., /en-US/products) (#86)

--- tests/VirtoCommerce.SitemapsModule.Tests/SitemapUrlBuilderTests.cs
+++ tests/VirtoCommerce.SitemapsModule.Tests/SitemapUrlBuilderTests.cs
@@ -16,18 +16,20 @@ public class SitemapUrlBuilderTests
     {
         Id = "s1",
         Url = "https://store.com",
-        Languages = ["en"],
-        DefaultLanguage = "en",
+        Languages = ["en-US"],
+        DefaultLanguage = "en-US",
         Catalog = "catalog1",
+        Settings = [],
     };
 
     private static readonly Store _multiLanguageStore = new()
     {
         Id = "s2",
         Url = "https://store.com",
-        Languages = ["en", "fr"],
-        DefaultLanguage = "en",
+        Languages = ["en-US", "fr-FR"],
+        DefaultLanguage = "en-US",
         Catalog = "catalog2",
+        Settings = [],
     };
 
     private static readonly TestEntity _entity = new()
@@ -37,19 +39,19 @@ public class SitemapUrlBuilderTests
             new SeoInfo
             {
                 StoreId = "s1",
-                LanguageCode = "en",
+                LanguageCode = "en-US",
                 SemanticUrl = "s1-en-entity",
             },
             new SeoInfo
             {
                 StoreId = "s2",
-                LanguageCode = "en",
+                LanguageCode = "en-US",
                 SemanticUrl = "s2-en-entity",
             },
             new SeoInfo
             {
                 StoreId = "s2",
-                LanguageCode = "fr",
+                LanguageCode = "fr-FR",
                 SemanticUrl = "s2-fr-entity",
             },
             new SeoInfo
@@ -65,19 +67,19 @@ public class SitemapUrlBuilderTests
                     {
                         Id = "catalog1",
                         SeoObjectType = "Catalog",
-                        SeoInfos = [new SeoInfo { StoreId = "s1", LanguageCode = "en", SemanticUrl = "s1-en-catalog1" }],
+                        SeoInfos = [new SeoInfo { StoreId = "s1", LanguageCode = "en-US", SemanticUrl = "s1-en-catalog1" }],
                     },
                     new OutlineItem
                     {
                         Id = "category1",
                         SeoObjectType = "Category",
-                        SeoInfos = [new SeoInfo { StoreId = "s1", LanguageCode = "en", SemanticUrl = "s1-en-category1" }],
+                        SeoInfos = [new SeoInfo { StoreId = "s1", LanguageCode = "en-US", SemanticUrl = "s1-en-category1" }],
                     },
                     new OutlineItem
                     {
                         Id = "category2",
                         SeoObjectType = "Category",
-                        SeoInfos = [new SeoInfo { StoreId = "s1", LanguageCode = "en", SemanticUrl = "s1-en-category2" }],
+                        SeoInfos = [new SeoInfo { StoreId = "s1", LanguageCode = "en-US", SemanticUrl = "s1-en-category2" }],
                     },
                 ],
             },
@@ -95,22 +97,22 @@ public class SitemapUrlBuilderTests
 
             new object[] { _singleLanguageStore, null,               null,    "",                          null,    "https://store.com/" },
             new object[] { _singleLanguageStore, null,               null,    "test",                      null,    "https://store.com/test" },
-            new object[] { _singleLanguageStore, null,               null,    "{slug}",                    _entity, "https://store.com/s1-en-category2" },
-            new object[] { _singleLanguageStore, null,               "en",    "{slug}",                    _entity, "https://store.com/s1-en-category2" },
-            new object[] { _singleLanguageStore, null,               "fr",    "{slug}",                    _entity, "https://store.com/s1-en-category2" },
-            new object[] { _singleLanguageStore, null,               "fr",    "{slug_short}",              _entity, "https://store.com/s1-en-category2" },
-            new object[] { _singleLanguageStore, null,               "fr",    "{slug_long}",               _entity, "https://store.com/s1-en-category1/s1-en-category2" },
+            new object[] { _singleLanguageStore, null,               null,    "{slug}",                    _entity, "https://store.com/s1-en-category1/s1-en-category2" },
+            new object[] { _singleLanguageStore, null,               "en-US",    "{slug}",                    _entity, "https://store.com/s1-en-category1/s1-en-category2" },
+            new object[] { _singleLanguageStore, null,               "fr-FR",    "{slug}",                    _entity, "https://store.com/s1-en-category1/s1-en-category2" },
+            new object[] { _singleLanguageStore, null,               "fr-FR",    "{slug_short}",              _entity, "https://store.com/s1-en-category2" },
+            new object[] { _singleLanguageStore, null,               "fr-FR",    "{slug_long}",               _entity, "https://store.com/s1-en-category1/s1-en-category2" },
 
-            new object[] { _multiLanguageStore,  null,               null,    "",                          null,    "https://store.com/en/" },
-            new object[] { _multiLanguageStore,  null,               null,    "test",                      null,    "https://store.com/en/test" },
-            new object[] { _multiLanguageStore,  null,               null,    "{slug}",                    _entity, "https://store.com/en/s2-en-entity" },
-            new object[] { _multiLanguageStore,  null,               "en",    "{slug}",                    _entity, "https://store.com/en/s2-en-entity" },
-            new object[] { _multiLanguageStore,  null,               "fr",    "{slug}",                    _entity, "https://store.com/fr/s2-fr-entity" },
-            new object[] { _multiLanguageStore,  null,               "en",    "",                          null,    "https://store.com/en/" },
-            new object[] { _multiLanguageStore,  null,               "fr",    "test",                      null,    "https://store.com/fr/test" },
-            new object[] { _multiLanguageStore,  null,               "de",    "test",                      null,    "https://store.com/en/test" },
-            new object[] { _multiLanguageStore,  null,               "en",    "https://absolute.com/test", _entity, "https://absolute.com/test" },
-            new object[] { _multiLanguageStore,  "https://base.com", "fr",    "{slug}",                    _entity, "https://base.com/fr/s2-fr-entity" },
+            new object[] { _multiLanguageStore,  null,               null,    "",                          null,    "https://store.com/" },
+            new object[] { _multiLanguageStore,  null,               null,    "test",                      null,    "https://store.com/test" },
+            new object[] { _multiLanguageStore,  null,               null,    "{slug}",                    _entity, "https://store.com/s2-en-entity" },
+            new object[] { _multiLanguageStore,  null,               "en-US",    "{slug}",                    _entity, "https://store.com/s2-en-entity" },
+            new object[] { _multiLanguageStore,  null,               "fr-FR",    "{slug}",                    _entity, "https://store.com/fr-FR/s2-fr-entity" },
+            new object[] { _multiLanguageStore,  null,               "en-US",    "",                          null,    "https://store.com/" },
+            new object[] { _multiLanguageStore,  null,               "fr-FR",    "test",                      null,    "https://store.com/fr-FR/test" },
+            new object[] { _multiLanguageStore,  null,               "de-DE",    "test",                      null,    "https://store.com/test" },
+            new object[] { _multiLanguageStore,  null,               "en-US",    "https://absolute.com/test", _entity, "https://absolute.com/test" },
+            new object[] { _multiLanguageStore,  "https://base.com", "fr-FR",    "{slug}",                    _entity, "https://base.com/fr-FR/s2-fr-entity" },
         };
 
     [Theory]
